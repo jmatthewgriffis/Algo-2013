6f8c88bba0ca1b381b498b1e72b81dbd70456fd5
J. Matthew Griffis <grifj153@newschool.edu>
2013-10-22 10:35:50 -0400

    Reordered the files (minor change).

--- of_v0.8.0_osx_release/apps/homework/Week8_vector_field_composition/src/testApp.h
+++ of_v0.8.0_osx_release/apps/homework/Week8_vector_field_composition/src/testApp.h
@@ -20,6 +20,10 @@ class testApp : public ofBaseApp{
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
     
+    // CHALLENGE: 1. Make two compositions using particles and vector fields, with an emphasis on the paths traveled by particles inside of the field.  In class, we saw particles moving through the vector fields, but didn't pay much attention to where they had come from and gone to.  Think about displaying the particles in a way besides just circles.  The vector field can be static or change over time.
+    
+    // NOTE: springboarding off Charlie's repo example for time efficiency's sake.
+    
     void addParticle();
     
     FlowField           myField;
